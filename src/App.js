44fc28d5f9bb6248a83ef92782a26f7f1b4cff95
gijsmaas82 <gijsmaas82@outlook.com>
2019-08-23 22:31:30 +0200

    step 1 and 2 finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = [
+  {
+    name: "Ivel Z3",
+    manufacturer: "Ivasim",
+    year: 1969,
+    origin: "Croatia"
+  },
+  {
+    name: "Bally Astrocade",
+    manufacturer: "Bally Consumer Products",
+    year: 1977,
+    origin: "USA"
+  },
+  {
+    name: "Sord M200 Smart Home Computer",
+    manufacturer: "Sord Computer Corporation",
+    year: 1971,
+    origin: "Japan"
+  },
+  {
+    name: "Commodore 64",
+    manufacturer: "Commodore",
+    year: 1982,
+    origin: "USA"
+  }
+]
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {value: ''}
+  }
+
+  updateSelection = (event) => {
+    this.setState({
+      value: event.target.value
+    })
+  }
+  
+  render() {
+    console.log(this.state)
+    return (
+      <div className="App">
+        <select onChange={this.updateSelection}>
+          <option value=''>---pick a model---</option>
+          {data.map(computer => {
+            return <option value={computer.name}>{computer.name}  ({computer.year})</option>
+          })}
+        </select>
+      </div>
+    )
+  }
 }
 
-export default App;
+
